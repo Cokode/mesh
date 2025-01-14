d5a22f6622cbd04c0f16353e1147a4a2696f7f64
Collins C. A. <contactmrc4@gmail.com>
2025-01-14 14:14:55 +0100

    Updat on Pressable props

--- src/components/textInput.js
+++ src/components/textInput.js
@@ -2,30 +2,28 @@ import React from "react";
 import { Text, View, TextInput, StyleSheet, Pressable } from "react-native";
 import EvilIcons from '@expo/vector-icons/EvilIcons';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import Feather from '@expo/vector-icons/Feather';
 import Octicons from '@expo/vector-icons/Octicons';
 
 
 
-const InputSection = ({CloseCard}) => {
-
+const InputSection = ({CloseCard, openModal}) => {
 
   return (
     <>
       <View style={{flexDirection: "row", alignContent: "space-around", paddingHorizontal: 20, paddingVertical: 8, marginBottom: 5, backgroundColor: "white" }}>
       
-        <Pressable onPress={CloseCard} style={{paddingRight: 50, flexDirection: "row", alignItems: "center"}} >
+        <Pressable onPress={CloseCard} style={{ paddingRight: 50, flexDirection: "row", alignItems: "center" }} >
           <AntDesign name="up" size={24} color="black" />
           <Text> Close </Text>
         </Pressable> 
 
-        <Pressable style={{paddingRight: 50, flexDirection: "row", alignItems: "center"}}>
+        <Pressable onPress={openModal} style={{ paddingRight: 50, flexDirection: "row", alignItems: "center" }}>
           <EvilIcons name="comment" size={24} color="black" />
           <Text> Comment </Text>
         </Pressable>
 
-        <Pressable style={{flexDirection: "row", alignItems: "center"}} >
-          <Octicons name="report" size={24} color="black" />
+        <Pressable style={{ flexDirection: "row", alignItems: "center" }} >
+          <Octicons name="report" size={20} color="black" />
           <Text> Report </Text>
         </Pressable>
       </View>
