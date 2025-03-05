79aa707b25012a73c94815848b0fafffc344d3f1
Collins C. A. <contactmrc4@gmail.com>
2025-03-05 23:04:12 +0100

    Added: CheerScreen, a display screen on sucessfuly barcode verification

---
+++ src/components/cheerScreen.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, Pressable, Button } from 'react-native';
+
+// Re-usable component, offers a founder option 
+// to contact owner of a lost item
+export default function CheerScreen ({ owner, itemName, styles }) {
+
+  return (
+    <View>
+      <Text>Congratulations! You just found {owner} {itemName } </Text>
+
+      <View>
+        <Text>lets let { owner } know you have it</Text>
+      </View>
+
+      <View>
+        <Pressable>
+          <Text>Send a Found Message</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
